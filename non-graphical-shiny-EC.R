32b3cf7fb52840d68afe254a6c4543727f265cab
Paul Heideman <pmheideman@gmail.com>
2020-10-12 11:15:10 -0400

    Add files via upload

--- non-graphical-shiny-EC.R
+++ non-graphical-shiny-EC.R
@@ -15,60 +15,96 @@ ui <- fluidPage(
   sidebarLayout(
     
     # State Vote Shares Sidebar ----
-    sidebarPanel("Democratic Vote Shares",
+    sidebarPanel("Democratic Share of Two-Party Vote",
                  
       # Input: Alabama Vote Share ----
       sliderInput("AL", "Alabama:",
                   min = 0, max = 100,
-                  value = 50, step = .1,
+                  value = 35.6, step = .1,
                   post="%"),
       
       # Input: Alaska Vote Share ----
       sliderInput("AK", "Alaska:",
                   min = 0, max = 100,
-                  value = 50, step = .1,
+                  value = 41.6, step = .1,
                   post="%"),
       
       # Input: Arizona Vote Share ----
       sliderInput("AZ", "Arizona:",
                   min = 0, max = 100,
-                  value = 50, step = .1,
+                  value = 48.1, step = .1,
                   post="%"),
       
       # Input: Arizona Vote Share ----
       sliderInput("AR", "Arkansas:",
                   min = 0, max = 100,
-                  value = 50, step = .1,
+                  value = 35.7, step = .1,
                   post="%"),
       
       # Input: California Vote Share ----
       sliderInput("CA", "California:",
                   min = 0, max = 100,
-                  value = 50, step=.1,
+                  value = 66.1, step=.1,
                   post="%"),
       
       # Input: Colorado Vote Share ----
       sliderInput("CO", "Colorado:",
                   min = 0, max = 100,
-                  value = 50, step=.1,
+                  value = 52.7, step=.1,
                   post="%"),
       
       # Input: Connecticut Vote Share ----
       sliderInput("CT", "Connecticut:",
                   min = 0, max = 100,
-                  value = 50, step=.1,
+                  value = 57.1, step=.1,
                   post="%"),
       
       # Input: Delaware Vote Share ----
       sliderInput("DE", "Delaware:",
                   min = 0, max = 100,
-                  value = 50, step=.1,
+                  value = 56, step=.1,
+                  post="%"),
+      
+      # Input: Florida Vote Share ----
+      sliderInput("FL", "Florida:",
+                  min = 0, max = 100,
+                  value = 49.4, step=.1,
+                  post="%"),
+      
+      # Input: Georgia Vote Share ----
+      sliderInput("GA", "Georgia:",
+                  min = 0, max = 100,
+                  value = 47.3, step=.1,
+                  post="%"),
+      
+      # Input: Hawaii Vote Share ----
+      sliderInput("HI", "Hawaii:",
+                  min = 0, max = 100,
+                  value = 67.4, step=.1,
+                  post="%"),
+      
+      # Input: Idaho Vote Share ----
+      sliderInput("ID", "Idaho:",
+                  min = 0, max = 100,
+                  value = 31.7, step=.1,
+                  post="%"),
+      
+      # Input: Illinois Vote Share ----
+      sliderInput("IL", "Illinois:",
+                  min = 0, max = 100,
+                  value = 59.0, step=.1,
+                  post="%"),
+      
+      # Input: Indiana Vote Share ----
+      sliderInput("IN", "Indiana:",
+                  min = 0, max = 100,
+                  value = 39.9, step=.1,
                   post="%"),
       
       # Input: New York Vote Share ----
       sliderInput("NY", "New York:",
                   min = 0, max = 100,
-                  value = 50, step = 0.1,
+                  value = 63.4, step = 0.1,
                   post="%")
       
     ),
@@ -113,6 +149,27 @@ server <- function(input, output) {
                                               if_else(input$CT>=50,
                                                       df.votes$ec.votes[df.votes$state=="CT"],
                                                       as.integer(0)) +
+                                              if_else(input$DE>=50,
+                                                      df.votes$ec.votes[df.votes$state=="DE"],
+                                                      as.integer(0)) +
+                                              if_else(input$FL>=50,
+                                                      df.votes$ec.votes[df.votes$state=="FL"],
+                                                      as.integer(0)) +
+                                              if_else(input$GA>=50,
+                                                      df.votes$ec.votes[df.votes$state=="GA"],
+                                                      as.integer(0)) +
+                                              if_else(input$HI>=50,
+                                                      df.votes$ec.votes[df.votes$state=="HI"],
+                                                      as.integer(0)) +
+                                              if_else(input$ID>=50,
+                                                      df.votes$ec.votes[df.votes$state=="ID"],
+                                                      as.integer(0)) +
+                                              if_else(input$IL>=50,
+                                                      df.votes$ec.votes[df.votes$state=="IL"],
+                                                      as.integer(0)) +
+                                              if_else(input$IN>=50,
+                                                      df.votes$ec.votes[df.votes$state=="IN"],
+                                                      as.integer(0)) +
                                               if_else(input$NY>=50,
                                                       df.votes$ec.votes[df.votes$state=="NY"],
                                                       as.integer(0))))
@@ -140,6 +197,27 @@ server <- function(input, output) {
                                              if_else(input$CT<50, 
                                                      df.votes$ec.votes[df.votes$state=="CT"], 
                                                      as.integer(0)) +
+                                             if_else(input$DE<50, 
+                                                     df.votes$ec.votes[df.votes$state=="DE"], 
+                                                     as.integer(0)) +
+                                             if_else(input$FL<50, 
+                                                     df.votes$ec.votes[df.votes$state=="FL"], 
+                                                     as.integer(0)) +
+                                             if_else(input$GA<50, 
+                                                     df.votes$ec.votes[df.votes$state=="GA"], 
+                                                     as.integer(0)) +
+                                             if_else(input$HI<50, 
+                                                     df.votes$ec.votes[df.votes$state=="HI"], 
+                                                     as.integer(0)) +
+                                             if_else(input$ID<50, 
+                                                     df.votes$ec.votes[df.votes$state=="ID"], 
+                                                     as.integer(0)) +
+                                             if_else(input$IL<50, 
+                                                     df.votes$ec.votes[df.votes$state=="IL"], 
+                                                     as.integer(0)) +
+                                             if_else(input$IN<50, 
+                                                     df.votes$ec.votes[df.votes$state=="IN"], 
+                                                     as.integer(0)) +
                                              if_else(input$NY<50, 
                                                      df.votes$ec.votes[df.votes$state=="NY"], 
                                                      as.integer(0))))
@@ -156,25 +234,46 @@ server <- function(input, output) {
                 "California",
                 "Colorado",
                 "Connecticut",
+                "Delaware",
+                "Florida",
+                "Georgia",
+                "Hawaii",
+                "Idaho",
+                "Illinois",
+                "Indiana",
                "New York"),
-      Dem.Pop = prettyNum(c(input$AL/100*df.votes$electorate[df.votes$state=="AL"],
+      Dem.Pop = prettyNum(round(c(input$AL/100*df.votes$electorate[df.votes$state=="AL"],
                             input$AK/100*df.votes$electorate[df.votes$state=="AK"],
                             input$AZ/100*df.votes$electorate[df.votes$state=="AZ"],
                             input$AR/100*df.votes$electorate[df.votes$state=="AR"],
                             input$CA/100*df.votes$electorate[df.votes$state=="CA"], 
                             input$CO/100*df.votes$electorate[df.votes$state=="CO"],
                             input$CT/100*df.votes$electorate[df.votes$state=="CT"],
-                            input$NY/100*df.votes$electorate[df.votes$state=="NY"]),
+                            input$DE/100*df.votes$electorate[df.votes$state=="DE"],
+                            input$FL/100*df.votes$electorate[df.votes$state=="FL"],
+                            input$GA/100*df.votes$electorate[df.votes$state=="GA"],
+                            input$HI/100*df.votes$electorate[df.votes$state=="HI"],
+                            input$ID/100*df.votes$electorate[df.votes$state=="ID"],
+                            input$IL/100*df.votes$electorate[df.votes$state=="IL"],
+                            input$IN/100*df.votes$electorate[df.votes$state=="IN"],
+                            input$NY/100*df.votes$electorate[df.votes$state=="NY"])),
                           big.mark = ",",
                           scientific=FALSE),
-      Rep.Pop = prettyNum(c((1-input$AL/100)*df.votes$electorate[df.votes$state=="AL"],
+      Rep.Pop = prettyNum(round(c((1-input$AL/100)*df.votes$electorate[df.votes$state=="AL"],
                             (1-input$AK/100)*df.votes$electorate[df.votes$state=="AK"],
                             (1-input$AZ/100)*df.votes$electorate[df.votes$state=="AZ"],
                             (1-input$AR/100)*df.votes$electorate[df.votes$state=="AR"],
                             (1-input$CA/100)*df.votes$electorate[df.votes$state=="CA"], 
                             (1-input$CO/100)*df.votes$electorate[df.votes$state=="CO"], 
-                            (1-input$CO/100)*df.votes$electorate[df.votes$state=="CT"], 
-                            (1-input$NY/100)*df.votes$electorate[df.votes$state=="NY"]),
+                            (1-input$CT/100)*df.votes$electorate[df.votes$state=="CT"], 
+                            (1-input$DE/100)*df.votes$electorate[df.votes$state=="DE"], 
+                            (1-input$FL/100)*df.votes$electorate[df.votes$state=="FL"], 
+                            (1-input$GA/100)*df.votes$electorate[df.votes$state=="GA"], 
+                            (1-input$HI/100)*df.votes$electorate[df.votes$state=="HI"],
+                            (1-input$HI/100)*df.votes$electorate[df.votes$state=="ID"],
+                            (1-input$IL/100)*df.votes$electorate[df.votes$state=="IL"],
+                            (1-input$IN/100)*df.votes$electorate[df.votes$state=="IN"],
+                            (1-input$NY/100)*df.votes$electorate[df.votes$state=="NY"])),
                           big.mark = ",",
                           scientific=FALSE),
       Dem.EC = c(if_else(input$AL>=50,
@@ -197,6 +296,27 @@ server <- function(input, output) {
                          as.integer(0)),
                  if_else(input$CT>=50,
                          df.votes$ec.votes[df.votes$state=="CT"],
+                         as.integer(0)),
+                 if_else(input$DE>=50,
+                         df.votes$ec.votes[df.votes$state=="DE"],
+                         as.integer(0)),
+                 if_else(input$FL>=50,
+                         df.votes$ec.votes[df.votes$state=="FL"],
+                         as.integer(0)),
+                 if_else(input$GA>=50,
+                         df.votes$ec.votes[df.votes$state=="GA"],
+                         as.integer(0)),
+                 if_else(input$HI>=50,
+                         df.votes$ec.votes[df.votes$state=="HI"],
+                         as.integer(0)),
+                 if_else(input$ID>=50,
+                         df.votes$ec.votes[df.votes$state=="ID"],
+                         as.integer(0)),
+                 if_else(input$IL>=50,
+                         df.votes$ec.votes[df.votes$state=="IL"],
+                         as.integer(0)),
+                 if_else(input$IN>=50,
+                         df.votes$ec.votes[df.votes$state=="IN"],
                          as.integer(0)),
                  if_else(input$NY>=50,
                          df.votes$ec.votes[df.votes$state=="NY"],
@@ -222,6 +342,27 @@ server <- function(input, output) {
                  if_else(input$CT<50, 
                          df.votes$ec.votes[df.votes$state=="CT"], 
                          as.integer(0)),
+                 if_else(input$DE<50, 
+                         df.votes$ec.votes[df.votes$state=="DE"], 
+                         as.integer(0)),
+                 if_else(input$FL<50, 
+                         df.votes$ec.votes[df.votes$state=="FL"], 
+                         as.integer(0)),
+                 if_else(input$GA<50, 
+                         df.votes$ec.votes[df.votes$state=="GA"], 
+                         as.integer(0)),
+                 if_else(input$HI<50, 
+                         df.votes$ec.votes[df.votes$state=="HI"], 
+                         as.integer(0)),
+                 if_else(input$ID<50, 
+                         df.votes$ec.votes[df.votes$state=="ID"], 
+                         as.integer(0)),
+                 if_else(input$IL<50, 
+                         df.votes$ec.votes[df.votes$state=="IL"], 
+                         as.integer(0)),
+                 if_else(input$IN<50, 
+                         df.votes$ec.votes[df.votes$state=="IN"], 
+                         as.integer(0)),
                  if_else(input$NY<50,
                          df.votes$ec.votes[df.votes$state=="NY"],
                          as.integer(0))),
@@ -230,7 +371,7 @@ server <- function(input, output) {
   })
   
   
-  # Show the values in an HTML table ----
+  # Render the table in an HTML table ----
   output$values <- renderTable({
     sliderValues()
   })
